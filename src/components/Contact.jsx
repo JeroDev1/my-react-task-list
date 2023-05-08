a9d1ddeb97bb5cafb20b298fb8f50aad5999156b
John Ramirez <elcuartoespadaulquiorra@gmail.com>
2023-05-08 00:03:26 -0500

    Integracion de barra lateras

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,59 @@
+import { useForm } from 'react-hook-form';
+
 export const Contact = () => {
-    return (
-        <h2>Formulario</h2>
-    )
-}
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = data => {
+    // Convertir los datos a JSON
+    const jsonData = JSON.stringify(data);
+
+    // Realizar la solicitud HTTP al backend
+    fetch('URL_DEL_BACKEND', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: jsonData
+    })
+      .then(response => {
+        if (response.ok) {
+          // La solicitud se realizó con éxito
+          console.log('Datos enviados a la base de datos');
+          // Aquí puedes agregar la lógica para enviar el correo si es necesario
+        } else {
+          // Ocurrió un error en la solicitud
+          console.log('Error al enviar los datos a la base de datos');
+        }
+      })
+      .catch(error => {
+        console.log('Error de red:', error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Formulario</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Nombre:</label>
+          <input {...register('nombre', { required: true })} />
+          {errors.nombre && <span>Este campo es requerido</span>}
+        </div>
+
+        <div>
+          <label>Correo:</label>
+          <input {...register('correo', { required: true, pattern: /^\S+@\S+$/i })} />
+          {errors.correo && <span>Ingrese un correo válido</span>}
+        </div>
+
+        <div>
+          <label>Asunto:</label>
+          <input {...register('asunto', { required: true })} />
+          {errors.asunto && <span>Este campo es requerido</span>}
+        </div>
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,61 +1,61 @@
-import { TaskList } from "./TaskList";
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { TaskList } from './TaskList';
 
-const tareasPendiente = [
+export const Home = () => {
+  const tareasPendientes = [
     { id: 1, descripcion: 'Recoger carro', completado: false },
     { id: 2, descripcion: 'Entregar trabajo', completado: false },
     { id: 3, descripcion: 'Comprar leche', completado: false },
   ];
 
-export const Home = () => {
+  const [listaTareas, setListaTareas] = useState(
+    JSON.parse(localStorage.getItem('listaTareas')) || tareasPendientes
+  );
+  const [nuevaTarea, setNuevaTarea] = useState('');
 
-    const [listaTareas, setListaTareas] = useState(
-        JSON.parse(localStorage.getItem('listaTareas')) || tareasPendiente
-      );
-      const [nuevaTarea, setNuevaTarea] = useState('');
-    
-      useEffect(() => {
-        localStorage.setItem('listaTareas', JSON.stringify(listaTareas));
-      }, [listaTareas]);
-    
-      const agregar = evento => {
-        evento.preventDefault();
-        if (nuevaTarea.trim() !== '') {
-          const nuevaTareaConId = {
-            id: new Date().getTime(),
-            descripcion: nuevaTarea,
-            completado: false,
-          };
-          setListaTareas([...listaTareas, nuevaTareaConId]);
-          setNuevaTarea('');
-        }
-      };
-    
-      const handleChange = evento => {
-        setNuevaTarea(evento.target.value);
-      };
-    
-      const eliminarTarea = id => {
-        const nuevasTareas = listaTareas.filter(tarea => tarea.id !== id);
-        setListaTareas(nuevasTareas);
-      };
-    
-      const editarTarea = (id, nuevaDescripcion) => {
-        const nuevasTareas = listaTareas.map(tarea => {
-          if (tarea.id === id) {
-            tarea.descripcion = nuevaDescripcion;
-          }
-          return tarea;
-        });
-        setListaTareas(nuevasTareas);
-      };
+  useEffect(() => {
+    localStorage.setItem('listaTareas', JSON.stringify(listaTareas));
+  }, [listaTareas]);
 
-    return (
-    <div >
+  const agregar = evento => {
+    evento.preventDefault();
+    if (nuevaTarea.trim() !== '') {
+      const nuevaTareaConId = {
+        id: new Date().getTime(),
+        descripcion: nuevaTarea,
+        completado: false,
+      };
+      setListaTareas([...listaTareas, nuevaTareaConId]);
+      setNuevaTarea('');
+    }
+  };
+
+  const handleChange = evento => {
+    setNuevaTarea(evento.target.value);
+  };
+
+  const eliminarTarea = id => {
+    const nuevasTareas = listaTareas.filter(tarea => tarea.id !== id);
+    setListaTareas(nuevasTareas);
+  };
+
+  const editarTarea = (id, nuevaDescripcion) => {
+    const nuevasTareas = listaTareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.descripcion = nuevaDescripcion;
+      }
+      return tarea;
+    });
+    setListaTareas(nuevasTareas);
+  };
+
+  return (
+    <div>
       <form className='F1'>
-        <input className='primerimput' 
+        <input
+          className='primerimput'
           type='text'
-          maxlength="25"
+          maxLength='25'
           value={nuevaTarea}
           onChange={handleChange}
           placeholder='Ingrese tarea'
@@ -70,5 +70,5 @@ export const Home = () => {
         onEditar={editarTarea}
       />
     </div>
-    )
-}
+  );
+};
