a8e970458ed6c947975d90f2b2c16e5480f9966e
John Ramirez <elcuartoespadaulquiorra@gmail.com>
2023-05-08 21:26:34 -0500

    formulario creado con validacion ruta contacto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,61 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { TaskList } from './TaskList';
+import { TaskList } from "./TaskList";
+import { useState, useEffect } from 'react';
 
-export const Home = () => {
-  const tareasPendientes = [
+const tareasPendiente = [
     { id: 1, descripcion: 'Recoger carro', completado: false },
     { id: 2, descripcion: 'Entregar trabajo', completado: false },
     { id: 3, descripcion: 'Comprar leche', completado: false },
   ];
 
-  const [listaTareas, setListaTareas] = useState(
-    JSON.parse(localStorage.getItem('listaTareas')) || tareasPendientes
-  );
-  const [nuevaTarea, setNuevaTarea] = useState('');
+export const Home = () => {
 
-  useEffect(() => {
-    localStorage.setItem('listaTareas', JSON.stringify(listaTareas));
-  }, [listaTareas]);
-
-  const agregar = evento => {
-    evento.preventDefault();
-    if (nuevaTarea.trim() !== '') {
-      const nuevaTareaConId = {
-        id: new Date().getTime(),
-        descripcion: nuevaTarea,
-        completado: false,
+    const [listaTareas, setListaTareas] = useState(
+        JSON.parse(localStorage.getItem('listaTareas')) || tareasPendiente
+      );
+      const [nuevaTarea, setNuevaTarea] = useState('');
+    
+      useEffect(() => {
+        localStorage.setItem('listaTareas', JSON.stringify(listaTareas));
+      }, [listaTareas]);
+    
+      const agregar = evento => {
+        evento.preventDefault();
+        if (nuevaTarea.trim() !== '') {
+          const nuevaTareaConId = {
+            id: new Date().getTime(),
+            descripcion: nuevaTarea,
+            completado: false,
+          };
+          setListaTareas([...listaTareas, nuevaTareaConId]);
+          setNuevaTarea('');
+        }
       };
-      setListaTareas([...listaTareas, nuevaTareaConId]);
-      setNuevaTarea('');
-    }
-  };
+    
+      const handleChange = evento => {
+        setNuevaTarea(evento.target.value);
+      };
+    
+      const eliminarTarea = id => {
+        const nuevasTareas = listaTareas.filter(tarea => tarea.id !== id);
+        setListaTareas(nuevasTareas);
+      };
+    
+      const editarTarea = (id, nuevaDescripcion) => {
+        const nuevasTareas = listaTareas.map(tarea => {
+          if (tarea.id === id) {
+            tarea.descripcion = nuevaDescripcion;
+          }
+          return tarea;
+        });
+        setListaTareas(nuevasTareas);
+      };
 
-  const handleChange = evento => {
-    setNuevaTarea(evento.target.value);
-  };
-
-  const eliminarTarea = id => {
-    const nuevasTareas = listaTareas.filter(tarea => tarea.id !== id);
-    setListaTareas(nuevasTareas);
-  };
-
-  const editarTarea = (id, nuevaDescripcion) => {
-    const nuevasTareas = listaTareas.map(tarea => {
-      if (tarea.id === id) {
-        tarea.descripcion = nuevaDescripcion;
-      }
-      return tarea;
-    });
-    setListaTareas(nuevasTareas);
-  };
-
-  return (
-    <div>
+    return (
+    <div >
       <form className='F1'>
-        <input
-          className='primerimput'
+        <input className='primerimput' 
           type='text'
-          maxLength='25'
+          maxLength="25"
           value={nuevaTarea}
           onChange={handleChange}
           placeholder='Ingrese tarea'
@@ -70,5 +70,5 @@ export const Home = () => {
         onEditar={editarTarea}
       />
     </div>
-  );
-};
+    )
+}
